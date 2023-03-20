0bb96b9ef284bf4ae329e9eb40144510698dc072
Raymond Somai <k00lrav@icloud.com>
2023-03-19 21:29:02 -0700

    updated css and details.js for backdrop

--- details/details.js
+++ details/details.js
@@ -1,7 +1,7 @@
 ///Raymond: this is for showing the backdrop and poster image
 const posterMovies = document.querySelector(".poster-container");
 const movieOverview = document.querySelector(".details-container");
-const movieContainer = document.querySelector(".movie-container");
+const backdrop = document.querySelector(".backdrop");
 
 fetch(
     "https://api.themoviedb.org/3/movie/now_playing?api_key=477f5f5debaf48768ed55d725362b931"
@@ -23,9 +23,9 @@ fetch(
   ///Raymond: takes the currently selected movie and loads the poster image
         let selectedMovie = movies.map((item) => {
             if(currentId == item.id){
-          return`
+          return`<div class="poster-image-container">
               <img src="https://image.tmdb.org/t/p/original/${item.poster_path}" class="poster-img" alt="">
-                `;
+                </div>`;
                 }
         }).join('');
         posterMovies.innerHTML = selectedMovie;
@@ -33,25 +33,25 @@ fetch(
 ///Raymond: movie details
         let movieDetails = movies.map((item) => {
           if(currentId == item.id){
-        return`
-                <h1 class="movie-title">${item.title}</h1>
+        return`<div class ="movie-info">
+                <h1>${item.title}</h1>
                 <p>${item.overview}</p>
                 <p>${item.release_date}</p>
                 <p>${item.vote_average} ⭐️</p>
-              `;
+              </div>`;
               }
       }).join('');
       movieOverview.innerHTML = movieDetails;
 
-///Raymond: backdrop image, currently commeneted out, Can not seem to figure out how to get the details and poster image to be placed onto of the images
-    //   let backDrop = movies.map((item) => {
-    //     if(currentId == item.id){
-    //   return`
-    //           <img src="https://image.tmdb.org/t/p/original/${item.backdrop_path}" class="backdrop-img" alt="">
-    //         `;
-    //         }
-    // }).join('');
-    // movieContainer.innerHTML = backDrop;
+///Raymond: backdrop image
+      let backDrop = movies.map((item) => {
+        if(currentId == item.id){
+      return`<div class="backdrop-container">
+              <img src="https://image.tmdb.org/t/p/original/${item.backdrop_path}" class="backdrop-img" alt="">
+            </div>`;
+            }
+    }).join('');
+    backdrop.innerHTML = backDrop;
 
     }
 
