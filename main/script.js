fd9ebd75c09fa6f80cbd4e1f267af898cfbdea1d
Raymond Somai <k00lrav@icloud.com>
2023-03-24 18:06:17 -0700

    downloaded latest update from Scott

--- main/script.js
+++ main/script.js
@@ -1,26 +1,43 @@
-const api = "https://api.themoviedb.org/3/movie/now_playing?api_key=477f5f5debaf48768ed55d725362b931";
-const searchURL =
-  "https://api.themoviedb.org/3/search/movie?api_key=477f5f5debaf48768ed55d725362b931";
+// I broke the API in parts. Better to understand- Fabio
+const api_base = "https://api.themoviedb.org/3"
+const api_key = "api_key=477f5f5debaf48768ed55d725362b931"
+const nowPlaying = "/movie/now_playing?"
+const bestRated = "/movie/top_rated?"
+const popular = "/movie/popular?"
+const upcoming = "/movie/upcoming?"
+
+const searchURL ="https://api.themoviedb.org/3/search/movie?api_key=477f5f5debaf48768ed55d725362b931";
 
 const title = document.querySelector(".title");
 const container = document.querySelector(".container");
 const form = document.querySelector("#form");
 const search = document.querySelector("#search");
 
-// API FETCH
-fetch(api)
-    .then((response) => response.json())
-    .then((data) => {
-      console.log(data);
-      
-      const movie = data.results;
-      sortAtoZ(movie);
-      sortByPopularity(movie);
-      sortByReleaseDate(movie);
-      sortByRating(movie);
-    })
-    .catch((err) => console.log(err));
+// dropdown buttons- Fabio
+const popularBtn = document.querySelector('#popular')
+const topRatedBtn = document.querySelector('#topRated')
+const upComingBtn = document.querySelector('#upcoming')
+const sortAtoZ = document.querySelector('#sortAtoZ')
 
+
+// API FETCH
+function getMovies(api){
+  fetch(api)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        
+        const movie = data.results;
+        // sortAtoZ(movie);
+        // sortByPopularity(movie);
+        // sortByReleaseDate(movie);
+        // sortByRating(movie);
+        cardSyntax(movie)
+      })
+      .catch((err) => console.log(err));
+  }
+    getMovies(api_base + nowPlaying + api_key)
+  
     
 // SORT/FILTER (NAV BAR)
 const sort = document.querySelector("#sort-filter");
@@ -37,13 +54,65 @@ function filter() {
 
 
 // CARD SYNTAX FUNCTION
-function cardSyntax(item) {
-  return ` 
+function cardSyntax(movies) {
+  let details = movies.map((item) => {
+    return` 
     <a class="card" href="../details/details.html?id=${item.id}">
-      <img src="https://image.tmdb.org/t/p/original/${item.poster_path}" alt="">
-      <div class="rating">${item.vote_average}</div>
-    </a>`;
+    <img src="https://image.tmdb.org/t/p/original/${item.poster_path}" alt="">
+    <div class="rating">${item.vote_average}</div>
+  </a>`
+  }).join('');
+  title.innerHTML = "Now Playing"
+  container.innerHTML = details;
 }
+
+//filter to Most Popular- Fabio
+function mostPopular(popular){
+  fetch(popular)
+     .then((response) => response.json())
+     .then((data) => {
+       const movie = data.results;
+       cardSyntax(movie)
+       title.innerHTML = 'Most Popular'
+     })
+     .catch((err) => console.log(err));
+   }   
+popularBtn.addEventListener('click', ()=> {
+  mostPopular(api_base + popular + api_key)
+})
+
+
+//filter to Best Rated- Fabio
+function topRated(bestRate){
+  fetch(bestRate)
+     .then((response) => response.json())
+     .then((data) => {
+       const movie = data.results;
+       cardSyntax(movie)
+       title.innerHTML = 'Best Rated'
+     })
+     .catch((err) => console.log(err));
+   }   
+topRatedBtn.addEventListener('click', ()=> {
+  topRated(api_base + bestRated + api_key)
+})
+
+
+//filter to Upcoming- Fabio
+function upComing(upcomig){
+  fetch(upcomig)
+     .then((response) => response.json())
+     .then((data) => {
+       const movie = data.results;
+       cardSyntax(movie)
+       title.innerHTML = 'Upcoming'
+     })
+     .catch((err) => console.log(err));
+   }  
+upComingBtn.addEventListener('click', ()=> {
+  upComing(api_base + upcoming + api_key)
+})
+
 
 
 
@@ -52,49 +121,34 @@ function cardSyntax(item) {
 // sortAtoZ can be updated to omit 'A' and 'The'.
 // ie, show 'The Whale' as 'W' in the alphabet instead of 'T'.
 // - Scott
-function sortAtoZ(movie) {
-  let details = movie.sort((a, b) => a.title.localeCompare(b.title)).map(item => cardSyntax(item)
-  ).join('');
-  title.innerHTML = `<h2>A to Z</h2>`;
-  container.innerHTML = details;
-}
-function sortByPopularity(movie) {
-  let details = movie.sort((a, b) => b.popularity - a.popularity).map(item => cardSyntax(item)
-  ).join('');
-  title.innerHTML = `<h2>Most Popular</h2>`;
-  container.innerHTML = details;
-}
-function sortByReleaseDate(movie) {
-  let details = movie.sort((a, b) => new Date(b.release_date) - new Date(a.release_date)).map(item => cardSyntax(item)
-  ).join('');
-  title.innerHTML = `<h2>New Releases</h2>`;
-  container.innerHTML = details;
-}
-function sortByRating(movie) {
-  let details = movie.sort((a, b) => b.vote_average - a.vote_average).map(item => cardSyntax(item)
-  ).join('');
-  title.innerHTML = `<h2>Best Rated</h2>`;
-  container.innerHTML = details;
-}
+// function sortAtoZ(movie) {
+//   let details = movie.sort((a, b) => a.title.localeCompare(b.title)).map(item => cardSyntax(item)
+//   ).join('');
+//   title.innerHTML = `<h2>A to Z</h2>`;
+//   container.innerHTML = details;
+// }
+// function sortByPopularity(movie) {
+//   let details = movie.sort((a, b) => b.popularity - a.popularity).map(item => cardSyntax(item)
+//   ).join('');
+//   title.innerHTML = `<h2>Most Popular</h2>`;
+//   container.innerHTML = details;
+// }
+// function sortByReleaseDate(movie) {
+//   let details = movie.sort((a, b) => new Date(b.release_date) - new Date(a.release_date)).map(item => cardSyntax(item)
+//   ).join('');
+//   title.innerHTML = `<h2>New Releases</h2>`;
+//   container.innerHTML = details;
+// }
+// function sortByRating(movie) {
+//   let details = movie.sort((a, b) => b.vote_average - a.vote_average).map(item => cardSyntax(item)
+//   ).join('');
+//   title.innerHTML = `<h2>Best Rated</h2>`;
+//   container.innerHTML = details;
+// }
 
 
 
-// SEARCH FUNCTION
-
-// Fabio, there is code above that uses a similar fetch request. Can this code be reduced? - Scott
-function getMovies(url) {
-  fetch(url)
-    .then((res) => res.json())
-    .then((json) => {
-      const movies = json;
-      cardSyntax(movies.results);
-      })
-      .catch((err) => console.log(err));
-
-  }
-
-getMovies(api);
-
+// SEARCH 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
 
@@ -102,7 +156,14 @@ form.addEventListener("submit", (e) => {
 
   if (searchValue) {
     getMovies(searchURL +"&query="+ searchValue);
+    search = '';
   } else {
-    getMovies(api);
+    getMovies(api_base + nowPlaying + api_key);
   }
 });
+
+
+// Observation:
+// -we are missing A to Z filter
+// -Title inside the grid is too small, maybe we can position it in the middle
+// -- After you type anything in the search, We have to remove the name from the input
