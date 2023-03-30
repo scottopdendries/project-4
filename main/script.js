0d31e8e3b7476860e339a993e74b02546729c285
scottopdendries <scottopdendries@gmail.com>
2023-03-30 17:55:56 -0600

    added media queries for tablet and phone

--- main/script.js
+++ main/script.js
@@ -54,6 +54,8 @@ function filter() {
 
 
 
+
+
 // CARD SYNTAX FUNCTION
 function cardSyntax(movies) {
   let details = movies.map((item) => {
@@ -113,39 +115,6 @@ function upComing(upcomig){
 upComingBtn.addEventListener('click', ()=> {
   upComing(api_base + upcoming + api_key)
 })
-
-
-
-
-// FILTER FUNCTIONS
-
-// sortAtoZ can be updated to omit 'A' and 'The'.
-// ie, show 'The Whale' as 'W' in the alphabet instead of 'T'.
-// - Scott
-// function sortAtoZ(movie) {
-//   let details = movie.sort((a, b) => a.title.localeCompare(b.title)).map(item => cardSyntax(item)
-//   ).join('');
-//   title.innerHTML = `<h2>A to Z</h2>`;
-//   container.innerHTML = details;
-// }
-// function sortByPopularity(movie) {
-//   let details = movie.sort((a, b) => b.popularity - a.popularity).map(item => cardSyntax(item)
-//   ).join('');
-//   title.innerHTML = `<h2>Most Popular</h2>`;
-//   container.innerHTML = details;
-// }
-// function sortByReleaseDate(movie) {
-//   let details = movie.sort((a, b) => new Date(b.release_date) - new Date(a.release_date)).map(item => cardSyntax(item)
-//   ).join('');
-//   title.innerHTML = `<h2>New Releases</h2>`;
-//   container.innerHTML = details;
-// }
-// function sortByRating(movie) {
-//   let details = movie.sort((a, b) => b.vote_average - a.vote_average).map(item => cardSyntax(item)
-//   ).join('');
-//   title.innerHTML = `<h2>Best Rated</h2>`;
-//   container.innerHTML = details;
-// }
 
 
 
